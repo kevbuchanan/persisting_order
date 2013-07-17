80e7918ca67bc4a81e0e7a365bc5287abf346a69
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-16 21:24:18 -0500

    added jquery to persist order

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,5 +4,9 @@ get '/' do
 end
 
 post '/items/order' do
-  
+  params[:order].each_with_index do |id, index|
+    item = Item.find(id)
+    item.update_attribute(:position, index.to_i)
+  end
+  redirect to('/')
 end
